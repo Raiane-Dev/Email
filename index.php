5c026294e348a3a0f1b107d57e180a614aa3d2a8
Raiane Dev <89032013+Raiane-Dev@users.noreply.github.com>
2022-01-02 21:31:56 -0300

    Esqueci rs.

--- index.php
+++ index.php
@@ -4,14 +4,6 @@
     session_start();
     date_default_timezone_set("America/Sao_Paulo");
 
-    if($_SERVER['SERVER_PORT'] === '8000'){
-        $_SESSION['id_user'] = 2;
-        $_SESSION['ip'] = "192.168.0.104";
-    } else {
-        $_SESSION['id_user'] = 3;
-        $_SESSION['ip'] = "192.168.0.107";
-    }
-
     App\Controllers\Controller::overall();
     $route = new App\Route\Router;
     $route->run('/home', '\App\Controllers\Controller', 'home');
